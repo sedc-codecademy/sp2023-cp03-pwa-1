b8c28e94067c791570996dc3c281bd7607154544
KireGjelev <gelevk@gmail.com>
2023-04-16 19:29:32 +0200

    Created Taskbar section with addition, edition and deletion tasks.

---
+++ src/index.js
@@ -0,0 +1,164 @@
+var i = 0;
+
+const settingsButtons = {
+    DELETE: "Delete",
+    EDIT: "Edit",
+    START: "Start"
+};
+
+openModal = function (li) {
+    if (li) {
+        const taskName = li.querySelector('#taskName');
+        const taskDesc = li.querySelector('#taskDesc');
+        const taskEst = li.querySelector('#taskEst');
+
+        document.getElementById("taskName").value = taskName.innerHTML;
+        document.getElementById("taskDesc").value = taskDesc.innerHTML;
+        document.getElementById("taskEst").value = taskEst.innerHTML;
+        i--;
+    }
+    document.getElementById("modal").style.display = "block";
+}
+
+closeModal = function () {
+    document.getElementById("taskName").value = "";
+    document.getElementById("taskDesc").value = "";
+    document.getElementById("taskEst").value = "";
+    document.getElementById("modal").style.display = "none";
+}
+
+
+addTask = function () {
+    const taskName = document.getElementById("taskName");
+    const taskEst = document.getElementById("taskEst");
+    const taskDesc = document.getElementById("taskDesc");
+
+
+    const text = taskDesc.value;
+
+    let spanName = createElement("span", taskName);
+    let spanEst = createElement("span", taskEst);
+    let spanDesc = createElement("div", taskDesc);
+
+    if (text.length > 30) {
+        spanDesc.classList = "hover-text";
+        let fullTooltipText = createElement("span", text);
+        fullTooltipText.innerHTML = text;
+        fullTooltipText.classList = "tooltip-text";
+        fullTooltipText.id = "bottom";
+        spanDesc.innerHTML = spanDesc.innerHTML.substr(0, 30);
+        spanDesc.appendChild(fullTooltipText);
+    }
+    let hr = document.createElement("hr");
+    hr.style.width = "300px";
+
+    spanDesc.appendChild(hr);
+    let div = createElement("div");
+    
+    div.appendChild(spanName);
+    div.appendChild(spanEst);
+    div.appendChild(createElement("br"));
+    div.appendChild(spanDesc);
+
+    let li = createElement("li");
+    let taskList = document.getElementById("taskList");
+    removeItemIfExists(taskList);
+
+    li.id = i++;
+    li.appendChild(div);
+    appendSettingsButton(li);
+
+    taskList.appendChild(li);
+    displayList(true);
+    closeModal();
+    if (taskList.querySelectorAll("li").length == 5) {
+        toggleAddButton(true);
+
+    }
+}
+
+removeItemIfExists = function (list) {
+    const listItems = list.querySelectorAll("li");
+    listItems.forEach((item) => {
+        if (item.id == i) {
+            list.removeChild(item);
+        }
+    });
+}
+
+createElement = function (element, input) {
+    if (!input) {
+        return document.createElement(element);
+    }
+
+    let newElement = document.createElement(element);
+    newElement.innerHTML = input.value;
+    newElement.id = input.name;
+    return newElement;
+}
+
+appendSettingsButton = function (li) {
+    let buttonDiv = document.createElement("div");
+    buttonDiv.classList = "actionButtons";
+
+    const button1 = addButton(li, settingsButtons.DELETE);
+    const button2 = addButton(li, settingsButtons.EDIT);
+    const button3 = addButton(li, settingsButtons.START);
+
+    buttonDiv.appendChild(button1);
+    buttonDiv.appendChild(button2);
+    buttonDiv.appendChild(button3);
+
+    li.appendChild(buttonDiv);
+}
+
+addButton = function (li, buttonType) {
+    let button = document.createElement("button");
+
+    button.innerHTML = buttonType;
+  
+    const buttonClass = buttonType.toLowerCase() === "delete" ?
+        "btn btn-outline-danger" : buttonType.toLowerCase() === "edit" ?
+            "btn btn-outline-warning" : "btn btn-outline-success";
+
+    button.classList = buttonClass;
+
+    switch (buttonType) {
+        case settingsButtons.DELETE:
+          
+            button.onclick = function () {
+                let ul = li.parentNode;
+                ul.removeChild(li);
+                if (ul.childElementCount == 0) {
+                    displayList(false);
+                }
+                toggleAddButton(false);
+            }
+            break;
+        case settingsButtons.EDIT:
+            button.onclick = function () {
+                openModal(li);
+            }
+            break;
+        case settingsButtons.START:
+            button.onclick = function () {
+                //TODO: implement start logic
+            }
+            break;
+    }
+    return button;
+}
+
+displayList = function (toggle) {
+    if (toggle) {
+        document.getElementById("tasks").style.display = "block";
+    } else {
+        document.getElementById("tasks").style.display = "none";
+    }
+}
+
+toggleAddButton = function (toggle) {
+    let taskList = document.getElementById("taskList");
+
+    document.getElementById("taskBtn").disabled = toggle;
+}
