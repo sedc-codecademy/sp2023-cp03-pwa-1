91182bcacb3ebec28c052192150694aad9d53922
Goce Kabov <gkabov@melontech.com>
2023-06-02 16:56:32 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ openModal = function (li) {
         document.getElementById("taskEst").value = taskEst.innerHTML;
     }
     document.getElementById("modal").style.display = "block";
+    sessions.push({name: "GOCE", date: "ASDASDAS", duration: "ASDSD"})
 }
 
 closeModal = function () {
@@ -227,23 +228,7 @@ var j = 0;
 //Nikola logic for sessions
 
 //test sessions
-var sessions = [
-    {
-      name: 'Session One',
-      date: '2023-06-01',
-      duration: '2 hours'
-    },
-    {
-      name: 'Session Two',
-      date: '2023-06-02',
-      duration: '1.5 hours'
-    },
-    {
-      name: 'Session Three',
-      date: '2023-06-03',
-      duration: '3 hours'
-    }
-  ];
+var sessions = [];
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -278,6 +263,77 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+
+let elements = {
+    page: document.querySelector("body").style.backgroundColor = "rgb(17, 54, 3)",
+    min: document.querySelector("#minutes"),
+    sec: document.querySelector("#seconds"),
+    work: document.querySelector("#workSession").
+        addEventListener("click", () => {
+            timer.stop();
+            document.querySelector('#start').innerHTML=`Start`;
+            timer.time = 45 * 60;
+            elements.min.innerHTML = "45";
+            elements.sec.innerHTML = "00";
+            document.querySelector("body").style.backgroundColor = "rgb(17, 54, 3)"
+        }
+        ),
+    long: document.querySelector("#longBreakSession").
+        addEventListener("click", () => {
+            timer.stop();
+            document.querySelector('#start').innerHTML=`Start`;
+            elements.min.innerHTML = "15";
+            elements.sec.innerHTML = "00";
+            timer.time = 15 * 60;
+            document.querySelector("body").style.backgroundColor = "#3f6c51";
+        }),
+    short: document.querySelector("#shortBreakSession").
+        addEventListener("click", () => {
+            timer.stop();
+            document.querySelector('#start').innerHTML=`Start`;
+            timer.time = 5 * 60;
+            elements.min.innerHTML = "05";
+            elements.sec.innerHTML = "00";
+            document.querySelector("body").style.backgroundColor = "#498467"
+        }),
+    start: document.querySelector("#start").
+        addEventListener("click", () => { 
+            if(timer.interval===null){
+            timer.start();
+            document.querySelector('#start').innerHTML=`Pause`
+            }
+            else {
+                timer.stop();
+                document.querySelector('#start').innerHTML=`Start`
+            }
+        }),
+
+}
+
+let timer = {
+    time: 2700,
+    interval: null,
+    start: function () {
+        this.interval = setInterval(() => {
+            if (this.time !== 0) {
+                this.time--;
+            }
+            convertFromSeconds();
+            
+        }, 1000)
+    },
+    stop: function () {
+        clearInterval(this.interval);
+        this.interval = null;
+    }
+}
+
+function convertFromSeconds() {
+    const minutes = Math.floor(timer.time / 60);
+    const seconds = timer.time % 60;
+    elements.min.textContent = minutes.toString().padStart(2, 0);
+    elements.sec.textContent = seconds.toString().padStart(2, 0);
+}
   
   
   
