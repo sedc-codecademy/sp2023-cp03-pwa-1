656b18bb8f726d3aa911f51e6612e12350bcb970
AlTomba <alberto.velkovski@gmail.com>
2023-06-07 08:01:48 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,254 @@
+var i = 0;
+
+const settingsButtons = {
+    DELETE: "Delete",
+    EDIT: "Edit",
+    START: "Start"
+};
+
+openModal = function (li) {
+    if (li) {
+        i = li.id
+        const taskName = li.querySelector('#taskName');
+        const taskDesc = li.querySelector('#taskDesc');
+        const taskEst = li.querySelector('#taskEst');
+        var taskDescText = "";
+        const bottomText = li.querySelector('#bottom');
+        if(bottomText)
+        {
+            taskDescText = bottomText.innerHTML
+        }
+        else
+        {
+            taskDescText = taskDesc.innerHTML;
+        }
+
+        document.getElementById("taskName").value = taskName.innerHTML;
+        document.getElementById("taskDesc").value = taskDescText;
+        document.getElementById("taskEst").value = taskEst.innerHTML;
+    }
+    document.getElementById("modal").style.display = "block";
+}
+
+closeModal = function () {
+    document.getElementById("taskName").value = "";
+    document.getElementById("taskDesc").value = "";
+    document.getElementById("taskEst").value = "";
+    document.getElementById("modal").style.display = "none";
+}
+
+
+addTask = function () {
+    const taskName = document.getElementById("taskName");
+    const taskEst = document.getElementById("taskEst");
+    const taskDesc = document.getElementById("taskDesc");
+
+    const text = taskDesc.value;
+
+    let spanName = createElement("span", taskName);
+    let spanEst = createElement("span", taskEst);
+    let spanDesc = createElement("div", taskDesc);
+    let div = createElement("div");
+    
+    div.appendChild(spanName);
+    div.appendChild(spanEst);
+    div.appendChild(createElement("br"));
+    div.appendChild(spanDesc);
+
+    if (text.length > 30) {
+        div.classList = "hover-text";
+        let fullTooltipText = createElement("span", text);
+        fullTooltipText.innerHTML = text;
+        fullTooltipText.classList = "tooltip-text";
+        fullTooltipText.id = "bottom";
+        spanDesc.innerHTML = spanDesc.innerHTML.substr(0, 30);
+        div.appendChild(fullTooltipText);
+    }
+    let hr = document.createElement("hr");
+    hr.style.width = "300px";
+    hr.style.color = "white";
+
+    div.appendChild(hr);
+    
+    let li = createElement("li");
+    let taskList = document.getElementById("taskList");
+    removeItemIfExists(taskList);
+
+    li.id = i++;
+    li.appendChild(div);
+    appendSettingsButton(li);
+
+    taskList.appendChild(li);
+    displayList(true);
+    closeModal();
+    if (taskList.querySelectorAll("li").length == 5) {
+        toggleAddButton(true);
+
+    }
+}
+
+removeItemIfExists = function (list) {
+    const listItems = list.querySelectorAll("li");
+    listItems.forEach((item) => {
+        if (item.id == i) {
+            list.removeChild(item);
+        }
+    });
+}
+
+createElement = function (element, input) {
+    if (!input) {
+        return document.createElement(element);
+    }
+
+    let newElement = document.createElement(element);
+    newElement.innerHTML = input.value;
+    newElement.id = input.name;
+    return newElement;
+}
+
+appendSettingsButton = function (li) {
+    let buttonDiv = document.createElement("div");
+    buttonDiv.classList = "actionButtons";
+
+    const button1 = addButton(li, settingsButtons.DELETE);
+    const button2 = addButton(li, settingsButtons.EDIT);
+    const button3 = addButton(li, settingsButtons.START);
+
+    buttonDiv.appendChild(button1);
+    buttonDiv.appendChild(button2);
+    buttonDiv.appendChild(button3);
+
+    li.appendChild(buttonDiv);
+}
+
+addButton = function (li, buttonType) {
+    let button = document.createElement("button");
+
+    button.innerHTML = buttonType;
+  
+    const buttonClass = buttonType.toLowerCase() === "delete" ?
+        "btn btn-outline-danger" : buttonType.toLowerCase() === "edit" ?
+            "btn btn-outline-warning" : "btn btn-outline-success";
+
+    button.classList = buttonClass;
+
+    switch (buttonType) {
+        case settingsButtons.DELETE:
+          
+            button.onclick = function () {
+                let ul = li.parentNode;
+                ul.removeChild(li);
+                i--;
+                if (ul.childElementCount == 0) {
+                    displayList(false);
+                }
+                toggleAddButton(false);
+            }
+            break;
+        case settingsButtons.EDIT:
+            button.onclick = function () {
+                openModal(li);
+            }
+            break;
+        case settingsButtons.START:
+            button.onclick = function () {
+                //TODO: implement start logic
+            }
+            break;
+    }
+    return button;
+}
+
+displayList = function (toggle) {
+    if (toggle) {
+        document.getElementById("tasks").style.display = "block";
+    } else {
+        document.getElementById("tasks").style.display = "none";
+    }
+}
+
+toggleAddButton = function (toggle) {
+    let taskList = document.getElementById("taskList");
+
+    document.getElementById("taskBtn").disabled = toggle;
+}
+
+//#region  timer 
+
+let elements = {
+    page: document.querySelector("body").style.backgroundColor = "rgb(17, 54, 3)",
+    min: document.querySelector("#minutes"),
+    sec: document.querySelector("#seconds"),
+    work: document.querySelector("#workSession").
+        addEventListener("click", () => {
+            timer.stop();
+            document.querySelector('#start').innerHTML=`Start`;
+            timer.time = 45 * 60;
+            elements.min.innerHTML = "45";
+            elements.sec.innerHTML = "00";
+            document.querySelector("body").style.backgroundColor = "rgb(17, 54, 3)"
+        }
+        ),
+    long: document.querySelector("#longBreakSession").
+        addEventListener("click", () => {
+            timer.stop();
+            document.querySelector('#start').innerHTML=`Start`;
+            elements.min.innerHTML = "15";
+            elements.sec.innerHTML = "00";
+            timer.time = 15 * 60;
+            document.querySelector("body").style.backgroundColor = "#3f6c51";
+        }),
+    short: document.querySelector("#shortBreakSession").
+        addEventListener("click", () => {
+            timer.stop();
+            document.querySelector('#start').innerHTML=`Start`;
+            timer.time = 5 * 60;
+            elements.min.innerHTML = "05";
+            elements.sec.innerHTML = "00";
+            document.querySelector("body").style.backgroundColor = "#498467"
+        }),
+    start: document.querySelector("#start").
+        addEventListener("click", () => { 
+            if(timer.interval===null){
+            timer.start();
+            document.querySelector('#start').innerHTML=`Pause`
+            }
+            else {
+                timer.stop();
+                document.querySelector('#start').innerHTML=`Start`
+            }
+        }),
+
+}
+
+let timer = {
+    time: 2700,
+    interval: null,
+    start: function () {
+        this.interval = setInterval(() => {
+            if (this.time !== 0) {
+                this.time--;
+            }
+            convertFromSeconds();
+            
+        }, 1000)
+    },
+    stop: function () {
+        clearInterval(this.interval);
+        this.interval = null;
+    }
+}
+
+function convertFromSeconds() {
+    const minutes = Math.floor(timer.time / 60);
+    const seconds = timer.time % 60;
+    elements.min.textContent = minutes.toString().padStart(2, 0);
+    elements.sec.textContent = seconds.toString().padStart(2, 0);
+}
+
+//#endregion
+
 //Start of Music Player
 //MP DOM
 let expandBtn = document.querySelector("#expand_btn");
@@ -52,7 +303,7 @@ let songImg = document.querySelector("#song_img");
 let currentSongName = document.querySelector("#p_song_name_id");
 let autoBtn = document.querySelector("#autoplay_btn");
 
-let timer;
+let timerMP;
 let autoplay = 0;
 
 let index_no = 0;
@@ -92,7 +343,7 @@ function load_track(index){
     songImg.src = all_songs[index].img
     currentSongName.innerHTML = all_songs[index].name
     track.load();
-    timer = setInterval(range_slider, 1000);
+    timerMP = setInterval(range_slider, 1000);
 }
 load_track(index_no);
 
@@ -307,5 +558,3 @@ function idTarget(event){
 
 addSongs();
 //End of Music Player
-
-
