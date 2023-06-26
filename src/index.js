28e3d19e3f09a7fc6d2138e9d8b26905de5559ec
Goce Kabov <gkabov@melontech.com>
2023-06-26 22:24:39 +0200

    demo changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 var i = 0;
+let tasks = [];
+let selectedTaskId = "";
+let mode = "work";
 
 const settingsButtons = {
     DELETE: "Delete",
@@ -19,51 +22,123 @@ closeModal = function () {
 
 
 addTask = function () {
-    const taskName = document.getElementById("taskName");
-    const taskEst = document.getElementById("taskEst");
-    const taskDesc = document.getElementById("taskDesc");
-
-    const text = taskDesc.value;
-
-    let spanName = createElement("span", taskName);
-    let spanEst = createElement("span", taskEst);
-    let spanDesc = createElement("div", taskDesc);
-    let div = createElement("div");
-    
-    div.appendChild(spanName);
-    div.appendChild(spanEst);
-    div.appendChild(createElement("br"));
-    div.appendChild(spanDesc);
-
-    if (text.length > 30) {
-        div.classList = "hover-text";
-        let fullTooltipText = createElement("span", text);
-        fullTooltipText.innerHTML = text;
-        fullTooltipText.classList = "tooltip-text";
-        fullTooltipText.id = "bottom";
-        spanDesc.innerHTML = spanDesc.innerHTML.substr(0, 30) + "...";
-        div.appendChild(fullTooltipText);
+    const taskName = document.getElementById("taskName").value;
+    const taskEst = document.getElementById("taskEst").value;
+    const taskDesc = document.getElementById("taskDesc").value;
+    const newTask = {
+        taskId: Math.random(),
+        taskName: taskName,
+        taskEst: taskEst,
+        taskDesc: taskDesc,
+        workTime: 45 * 60,
+        longbreak: 15 * 60,
+        shortBreak: 5 * 60
     }
-    let hr = document.createElement("hr");
-    hr.style.width = "300px";
-    hr.style.color = "white";
+    tasks.push(newTask);
+    console.log(tasks)
+    renderTasks();
+}
 
-    div.appendChild(hr);
-    
-    let li = createElement("li");
+
+const renderTasks = () => {
     let taskList = document.getElementById("taskList");
+    taskList.innerHTML = ""; 
+    tasks.forEach(task => {
+        const taskName = task.taskName;
+        const taskEst = task.taskEst;
+        const taskDesc = task.taskDesc;
+        const spanName = createElement("span");
+        spanName.textContent = taskName;
+        let spanEst = createElement("span", taskEst);
+        spanEst.textContent = taskEst;
+        let spanDesc = createElement("div");
+        spanDesc.textContent = taskDesc;
+        let div = createElement("div");
+        
+        div.appendChild(spanName);
+        div.appendChild(spanEst);
+        div.appendChild(createElement("br"));
+        div.appendChild(spanDesc);
 
-    li.id = i++;
-    li.appendChild(div);
-    appendSettingsButton(li);
+        if (taskDesc.length > 30) {
+            div.classList = "hover-text";
+            let fullTooltipText = createElement("span");
+            fullTooltipText.textContent = taskDesc;
+            fullTooltipText.classList = "tooltip-text";
+            fullTooltipText.id = "bottom";
+            spanDesc.innerHTML = spanDesc.innerHTML.substr(0, 30) + "...";
+            div.appendChild(fullTooltipText);
+        }
+        let hr = document.createElement("hr");
+        hr.style.width = "300px";
+        hr.style.color = "white";
 
-    taskList.appendChild(li);
-    displayList(true);
-    closeModal();
-    if (taskList.querySelectorAll("li").length == 5) {
-        toggleAddButton(true);
+        div.appendChild(hr);
+        
+        let li = createElement("li");
+        
 
-    }
+        li.id = i++;
+        li.appendChild(div);
+        appendSettingsButton(li);
+        li.addEventListener("click",() => {
+            selectedTaskId = task.taskId;
+            let elements = {
+                page: document.querySelector("body").style.backgroundColor = "rgb(17, 54, 3)",
+                min: document.querySelector("#minutes"),
+                sec: document.querySelector("#seconds"),
+                work: document.querySelector("#workSession").
+                    addEventListener("click", () => {
+                        timer.stop();
+                        document.querySelector('#start').innerHTML=`Start`;
+                        timer.time = tasks.find(task => task.taskId === selectedTaskId).workTime;
+                        elements.min.innerHTML = tasks.find(task => task.taskId === selectedTaskId).workTime;
+                        elements.sec.innerHTML = tasks.find(task => task.taskId === selectedTaskId).workTime;
+                        document.querySelector("body").style.backgroundColor = "rgb(17, 54, 3)"
+                    }
+                    ),
+                long: document.querySelector("#longBreakSession").
+                    addEventListener("click", () => {
+                        timer.stop();
+                        document.querySelector('#start').innerHTML=`Start`;
+                        elements.min.innerHTML = "15";
+                        elements.sec.innerHTML = "00";
+                        timer.time = 15 * 60;
+                        document.querySelector("body").style.backgroundColor = "#3f6c51";
+                    }),
+                short: document.querySelector("#shortBreakSession").
+                    addEventListener("click", () => {
+                        timer.stop();
+                        document.querySelector('#start').innerHTML=`Start`;
+                        timer.time = 5 * 60;
+                        elements.min.innerHTML = "05";
+                        elements.sec.innerHTML = "00";
+                        document.querySelector("body").style.backgroundColor = "#498467"
+                    }),
+                start: document.querySelector("#start").
+                    addEventListener("click", () => { 
+                        if(timer.interval===null){
+                        timer.start();
+                        document.querySelector('#start').innerHTML=`Pause`
+                        }
+                        else {
+                            timer.stop();
+                            document.querySelector('#start').innerHTML=`Start`
+                        }
+                    }),
+            
+            }
+        })
+
+        taskList.appendChild(li);
+        displayList(true);
+        closeModal();
+        if (taskList.querySelectorAll("li").length == 5) {
+            toggleAddButton(true);
+
+        }
+    })
+    console.log(selectedTaskId)
 }
 
 createElement = function (element, input) {
@@ -432,51 +507,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 //fix this part
-let elements = {
-    page: document.querySelector("body").style.backgroundColor = "rgb(17, 54, 3)",
-    min: document.querySelector("#minutes"),
-    sec: document.querySelector("#seconds"),
-    work: document.querySelector("#workSession").
-        addEventListener("click", () => {
-            timer.stop();
-            document.querySelector('#start').innerHTML=`Start`;
-            timer.time = 45 * 60;
-            elements.min.innerHTML = "45";
-            elements.sec.innerHTML = "00";
-            document.querySelector("body").style.backgroundColor = "rgb(17, 54, 3)"
-        }
-        ),
-    long: document.querySelector("#longBreakSession").
-        addEventListener("click", () => {
-            timer.stop();
-            document.querySelector('#start').innerHTML=`Start`;
-            elements.min.innerHTML = "15";
-            elements.sec.innerHTML = "00";
-            timer.time = 15 * 60;
-            document.querySelector("body").style.backgroundColor = "#3f6c51";
-        }),
-    short: document.querySelector("#shortBreakSession").
-        addEventListener("click", () => {
-            timer.stop();
-            document.querySelector('#start').innerHTML=`Start`;
-            timer.time = 5 * 60;
-            elements.min.innerHTML = "05";
-            elements.sec.innerHTML = "00";
-            document.querySelector("body").style.backgroundColor = "#498467"
-        }),
-    start: document.querySelector("#start").
-        addEventListener("click", () => { 
-            if(timer.interval===null){
-            timer.start();
-            document.querySelector('#start').innerHTML=`Pause`
-            }
-            else {
-                timer.stop();
-                document.querySelector('#start').innerHTML=`Start`
-            }
-        }),
 
-}
 
 let timer = {
     time: 2700,
@@ -1060,3 +1091,5 @@ window.addEventListener("resize", function(){
     }
 });
 //End of Music Player
+
+renderTasks();
